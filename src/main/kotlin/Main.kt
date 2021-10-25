4c373e86e183262bd42ca5bc8b01e4376352ebe8
James-Santana-Dev <jamesantana@gmail.com>
2021-10-25 16:37:56 -0300

    Primeiro Commi

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var nomeUsuario = "João"
+    var idade = 25
+    var preferenciaEmprego = "enfermeiro"
+    var nacionalidade = "brasileiro"
+    var tipoSanguineo = "O-"
+    var ano = 2021
+
+    println("Bom dia, me chamo $nomeUsuario, tenho $idade anos de idade, pois nasci em ${ano - idade}, busco um emprego de $preferenciaEmprego," +
+            " sou $nacionalidade e meu tipo sanguíneo é $tipoSanguineo.")
+
+
+
+
+}
